bed22d35f248bc400ba5fe6f39374664d716fe6d
Bart Kung <bartkung@gmail.com>
2023-09-01 14:58:24 -0700

    started a new version using tut

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.send('Hello from Yelp Camp');
+});
+
 app.get('/dog', (req, res) => {
     res.send('woof');
 });
