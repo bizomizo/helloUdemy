4aa5d51cfe2d5f2a163484fc2e0344f18474d9ad
Bart Kung <bartkung@gmail.com>
2023-09-01 15:11:00 -0700

    set up rendering with ejs views on home page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello from Yelp Camp');
+    res.render('home');
 });
 
 app.get('/dog', (req, res) => {
