00563825010fe0b1b3e7c4abfad2020f05db29d2
Bart Kung <bartkung@gmail.com>
2023-09-05 15:35:59 -0700

    added new and show pages

--- app.js
+++ app.js
@@ -17,16 +17,38 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')  //127.0.0.1 is same as 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));  //this is related to the app.post below to parse the req.body
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'my backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', {campgrounds});
 });
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000");
