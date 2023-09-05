720e08130fc11808eaa48c32a54a82f1851333ef
Bart Kung <bartkung@gmail.com>
2023-09-05 16:19:46 -0700

    implemented full CRUD framework

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')  //127.0.0.1 is same as 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));  //this is related to the app.post below to parse the req.body
+app.use(methodOverride('_method'));             //this is for faking the method=POST in the edit.ejs page into a PUT or PATCH form method in the app.put below
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -45,7 +47,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', {campground});
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+});
 
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
 
 
 
