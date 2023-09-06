e34397e98c157876de5a71833df05fa8ed34e9bf
Bart Kung <bartkung@gmail.com>
2023-09-05 17:08:20 -0700

    added some comments to common element questions

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', async (req, res) => {   //page reference on the server requires the "/" to start the path
     const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', {campgrounds});
+    res.render('campgrounds/index', {campgrounds});     //ejs with path set above starts looking in the views folder, so don't add "/" to path
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -42,20 +42,23 @@ app.post('/campgrounds', async (req, res) => {
 
 
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+app.get('/campgrounds/:id', async (req, res) => {           // the url will include the id from the db, the ":" will store this value within req.params.id
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
     res.render('campgrounds/show', {campground});
 });
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
     res.render('campgrounds/edit', {campground});
 });
 
 app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
+    const {id} = req.params;                        //this format is called destructuring an object, within req.params a property id exists and this will pull this property out to a variable named 'id'
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground}); // the "..." is called a spread operator and will copy all of an existing object into the function
+    res.redirect(`/campgrounds/${campground._id}`);     //the "_id" is inherent to every entry created in the database and automatically assigned an id value stored under _id
+    
 });
 
 app.delete('/campgrounds/:id', async (req, res) => {
